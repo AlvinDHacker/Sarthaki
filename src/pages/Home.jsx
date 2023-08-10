0ffbe9f6f7c0c021cd06e08712f6ce6fd2e44586
Alston <alstonsoares17@gmail.com>
2023-08-10 20:49:47 +0530

    Added initial contact page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 import About from '../components/AboutUs/About'
 import Carousel from '../components/Home/Carousel'
->>>>>>> 8de424c4526e935bf4f97c5746fb43bab51bd86e
 import Hero from '../components/Home/Hero'
 import Slider from '../components/Home/Slider'
 
@@ -24,14 +21,11 @@ function Home() {
   return (
     <>
       <Hero/>
-<<<<<<< HEAD
-=======
       <About/>
       <Slider />
       <div className='w-[350px] h-[190px] xs:w-[450px] xs:h-[250px] sm:w-[500px] sm:h-[280px] lg:w-[1210px] md:h-[620px] md:w-[1020px] mt-5' style={containerStyles}>
         <Carousel slides={slides} />
       </div>
->>>>>>> 8de424c4526e935bf4f97c5746fb43bab51bd86e
     </>
   )
 }
